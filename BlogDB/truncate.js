accb0ab67f3c05707586d983d2185223991f1fde
shruthanr <40868631+shruthanr@users.noreply.github.com>
2019-07-20 15:57:21 +0530

    Add files

---
+++ BlogDB/truncate.js
@@ -0,0 +1,11 @@
+module.export = truncateText;
+
+function truncateText(selector, maxLength) {
+    var element = document.querySelector(selector),
+        truncated = element.innerText;
+
+    if (truncated.length > maxLength) {
+        truncated = truncated.substr(0,maxLength) + '...';
+    }
+    return truncated;
+}
